9683dfbefa06eeafce9a729e42682a79e9978fec
fohlle <patte2000@hotmail.com>
2021-04-28 18:22:52 +0200

    forms done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,15 @@
 class UsersController < ApplicationController
 
-  def new
+  def index
+    @user = User.all
+  end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
   end
 
   def create
@@ -13,6 +21,21 @@ class UsersController < ApplicationController
       redirect_to new_user_path
     else
       render :new
+    end
+
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render :edit
     end
 
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
-  resources :users, only: [:new, :create]
+  root "users#index"
+
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
 
